ba0b8efa45700e7dba7868b814a8c2b2209b4710
Elliott Holt <elliottholt90@gmail.com>
2024-05-14 23:06:30 +0100

    Initial commit

---
+++ scripts/player.gd
@@ -0,0 +1,49 @@
+extends CharacterBody2D
+
+const scaling_factor = 2
+
+const SPEED = 300.0 * scaling_factor
+const JUMP_VELOCITY = -400.0 * scaling_factor
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") * scaling_factor
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	set_animation()
+	move_and_slide()
+	
+func set_animation():
+	if is_on_floor():
+		if abs(velocity.x) > 0:
+			animated_sprite_2d.play("running")
+		else:
+			animated_sprite_2d.play("idle")
+	else:
+		if velocity.y > 0:
+			animated_sprite_2d.play("falling")
+		else:
+			animated_sprite_2d.play("jumping")
